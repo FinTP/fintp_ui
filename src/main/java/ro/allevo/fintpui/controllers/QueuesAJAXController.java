3e954234a3073df5ee96d8ccf8395362a969025a
remusdecea <remus.decea@gmail.com>
2014-02-11 14:05:18 +0200

    added queues page populated using jQuery

--- src/main/java/ro/allevo/fintpui/controllers/QueuesAJAXController.java
+++ src/main/java/ro/allevo/fintpui/controllers/QueuesAJAXController.java
@@ -1,5 +1,7 @@
 package ro.allevo.fintpui.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/queuesAJAX")
 public class QueuesAJAXController {
-
+	
+	private static Logger logger = LogManager.getLogger(QueuesAJAXController.class
+			.getName());
 	@RequestMapping(method = RequestMethod.GET)
 	public String printMenu(ModelMap model) {
-		return "queuesAJAX";
+		logger.info("queuesAJAX requested");
+		return "tiles/queuesAJAX";
 	}
 }
