7ebf0e785246e3deb4f338ef860cd478d82a6684
remusdecea <remus.decea@gmail.com>
2014-03-11 17:11:31 +0200

    added queuesDynamic removed by mistake

---
+++ src/main/webapp/scripts/queuesDynamic.js
@@ -0,0 +1,46 @@
+var urlRoot = "http://localhost:8080/fintpWebServices/api/";
+var refreshSeconds = 30;
+var timeOut = refreshSeconds * 1000;
+
+
+$(function() {
+	ajaxGetQueues();
+	setInterval(function() {
+		console.log("update");
+		ajaxGetQueues();
+	}, timeOut);
+});
+
+function ajaxGetQueues(){
+	var url = urlRoot + "queues";
+	var object = new Object();
+	object.queues = getQueuesArray();
+	var dataJSON = JSON.stringify(object);
+	
+	$.ajax({
+		
+		data: {data: dataJSON, name:'test'},
+		method : 'POST',
+		url : "queuesDynamic",
+		dataType : 'json',
+		async : true,
+		success : function(response) {
+			console.log(response);
+			var numbers = response.numbers;
+			for(var index = 0; index < numbers.length; index++){
+				$("#queuesTable tr:nth-child("+ (index + 2) +") td:nth-child(4)").text(numbers[index]);
+			}
+		},
+		error : function(jqXHR, text, errorThrown) {
+			//HandleAjaxError(jqXHR, text, errorThrown);
+		}
+	});
+}
+
+function getQueuesArray(){
+	var data = [];
+	$("#queuesTable tr td:first-child").each(function(){
+		data.push($(this).text());
+	});
+	return data;
+}
